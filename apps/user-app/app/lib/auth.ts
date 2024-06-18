9d960f3a0b3bb72fba5e93d7494278099ff0a5ca
rakesh-paulraj1 <rakeshtherock2004@gmail.com>
2024-06-18 17:55:50 +0530

    commit

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -2,6 +2,8 @@ import db from "@repo/db/client";
 import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt";
 
+
+
 export const authOptions = {
     providers: [
       CredentialsProvider({
