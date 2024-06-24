1ec13d74b906432400106cb90a82197c3f413a11
rakesh-paulraj1 <rakeshtherock2004@gmail.com>
2024-06-24 12:45:39 +0530

    space notenough in ec2

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -1,6 +1,8 @@
 
 export default function() {
     return <div>
-        Dashboard
+        Insigts of the Payemts 
+        Please look into p2p transactions ,transactionsand transfer 
+        
     </div>
 }
