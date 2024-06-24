55bddfae0701808b7561513779f0ea60c8446239
rakesh-paulraj1 <rakeshtherock2004@gmail.com>
2024-06-24 12:52:46 +0530

    dashboard

--- apps/user-app/app/(dashboard)/dashboard/page.tsx
+++ apps/user-app/app/(dashboard)/dashboard/page.tsx
@@ -3,6 +3,7 @@ export default function() {
     return <div>
         Insigts of the Payemts 
         Please look into p2p transactions ,transactionsand transfer 
+        You can login using 2222222222 as phone number and bob as pasword to be other user 
         
     </div>
 }
